7e2bf5b7ddd41a470cfb688d478e1b05f86d1895
Michał Sibik <chudymichal123@gmail.com>
2019-08-09 00:39:43 +0200

    basic recipe list

--- src/Search.js
+++ src/Search.js
@@ -7,7 +7,7 @@ class Search extends React.Component{
         this.state = {apiRes: ''};
     }
 
-    click(e){
+    click = (e) => {
         e.preventDefault();
         var q = document.getElementById('query').value;
         fetch(`https://api.spoonacular.com/recipes/search?apiKey=${process.env.REACT_APP_API_KEY}&query=${q}&number=3`, {
@@ -15,17 +15,36 @@ class Search extends React.Component{
         }).then(res => {
             return res.json();
         }).then(res => {
-            console.log(res);
+            this.setState({apiRes: res});
         });
     }
     
     render(){
+        var resultList = '';
+        if(this.state.apiRes.results){
+            var results = this.state.apiRes.results;
+            resultList = (
+                <div className="container mt-2">
+                    <ul className="resultsList">
+                        {results.map((val) => {
+                            return (
+                                <a href="#">
+                                    <li key={val.id}>{val.title}</li>
+                                </a>
+                            );
+                        })}
+                    </ul>
+                </div>
+            );
+        }
+
         return(
             <div className="container-fluid mt-2">
                 <form className="form-inline justify-content-center">
                     <input id="query" className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button onClick={this.click} className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
+                {resultList}
             </div>
         );
     }
