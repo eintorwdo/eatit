cc869dfda7ec2d2cedc2ba90b1fbe7c348669b4e
Michał Sibik <chudymichal123@gmail.com>
2019-08-16 02:07:44 +0200

    switched from getDerivedStateFromProps to componentDidUpdate

--- src/Search.js
+++ src/Search.js
@@ -4,9 +4,9 @@ import queryString from 'query-string'
 class Search extends React.Component{
 
     constructor(props){
+        console.log("CONSTR");
         super(props);
         this.state = {apiRes: '', query: ''};
-
         var q = queryString.parse(this.props.location.search);
         // this.setState({query: this.props.location.search});
         this.state.query = this.props.location.search;
@@ -15,41 +15,45 @@ class Search extends React.Component{
         }).then(res => {
             return res.json();
         }).then(res => {
-            // this.setState({apiRes: res});
-            this.state.apiRes = res;
+            this.setState({apiRes: res});
+            // this.state.apiRes = res;
         });
     }
 
-    static getDerivedStateFromProps(props, state){
-        var cur = props.location.search;
-        console.log(props.location.search);
+    componentDidUpdate(prevProps, prevState){
+        var cur = this.props.location.search;
+        console.log(this.props.location.search, this.state.query);
         var prev = '';
-        if(state.query){
-            prev = state.query;
+        if(this.state.query){
+            prev = this.state.query;
         }
         if(cur !== prev){
-            // var q = queryString.parse(cur);
-            // fetch(`https://api.spoonacular.com/recipes/search?apiKey=${process.env.REACT_APP_API_KEY}&query=${q.query}&number=3`, {
-            // mode: 'cors'
-            // }).then(res => {
-            //     return res.json();
-            // }).then(res => {
-            //     return {apiRes: res};
-            // });
-            return {apiRes: '', query: cur};
+            var q = queryString.parse(cur);
+            fetch(`https://api.spoonacular.com/recipes/search?apiKey=${process.env.REACT_APP_API_KEY}&query=${q.query}&number=3`, {
+            mode: 'cors'
+            }).then(res => {
+                return res.json();
+            }).then(res => {
+                console.log("z fetchem", res);
+                this.setState({apiRes: res, query: cur});
+            });       
+        }
+        else{
+            console.log("bez fetcha");
         }
     }
-    
+
     render(){
         var resultList = '';
         if(this.state.apiRes.results){
             var results = this.state.apiRes.results;
+            // console.log(results);
             resultList = (
                 <div className="container mt-2">
                     <ul className="resultsList">
                         {results.map((val) => {
                             return (
-                                <a href="#">
+                                <a href="#" key={val.id}>
                                     <li key={val.id}>{val.title}</li>
                                 </a>
                             );
