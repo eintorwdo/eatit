053bf8c780ce49c9036c676b5d24bdf0aec9129b
Michał Sibik <chudymichal123@gmail.com>
2019-09-23 02:43:04 +0200

    instructions added

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,36 +3,64 @@ import React from 'react';
 class Recipe extends React.Component{
     constructor(props){
         super(props);
-        this.state = {ingredients: ''};
+        this.state = {ingredients: '', instructions: ''};
         fetch(`https://api.spoonacular.com/recipes/${this.props.match.params.id}/ingredientWidget.json?apiKey=${process.env.REACT_APP_API_KEY}`).then(res => {
             return res.json();
         }).then(res => {
-            // console.log(res);
-            this.setState({ingredients: res});
+            fetch(`https://api.spoonacular.com/recipes/${this.props.match.params.id}/analyzedInstructions?apiKey=${process.env.REACT_APP_API_KEY}`).then(async (response) => {
+                var inst = await response.json();
+                console.log(inst[0].steps);
+                this.setState({ingredients: res.ingredients, instructions: inst[0].steps});
+            })
         });
     }
 
     render(){
         var ingredients = this.state.ingredients;
-        // console.log(ingredients.ingredients.length);
+        var instructions = this.state.instructions;
         var ingredientList;
-        if(ingredients.ingredients){
+        var instructionsList;
+
+        if(ingredients){
             ingredientList = (
-                <div>
-                    <p>Ingredients:</p>
-                    <ul>
-                        {ingredients.ingredients.map(ingredient => {
-                        return(
-                                <li>{`${ingredient.name}: ${ingredient.amount.metric.value}${ingredient.amount.metric.unit}`}</li>
-                        );
-                    })}
-                    </ul>
+                <div className='row'>
+                    <div className='col-md'>
+                        <p style={{textAlign: "center"}}><strong>Ingredients</strong></p>
+                        <ul className='ingredientsList'>
+                            {ingredients.map(ingredient => {
+                            return(
+                                    <li>{`${ingredient.name}: ${ingredient.amount.metric.value} ${ingredient.amount.metric.unit}`}</li>
+                            );
+                        })}
+                        </ul>
+                    </div>
                 </div>
             )
         }
+
+        if(instructions){
+            instructionsList = (
+                <div className='row'>
+                    <div className='col-md-6 offset-md-3'>
+                        <p style={{textAlign: "center"}}><strong>Instructions:</strong></p>
+                        <ul className='ingredientsList'>
+                            {instructions.map(instruction => {
+                            return(
+                                    <li style={{marginBottom: "20px"}}>
+                                        <p style={{textAlign: "center"}}><b>Step {`${instruction.number}`}</b></p>
+                                        {`${instruction.step}`}
+                                    </li>
+                            );
+                        })}
+                        </ul>
+                    </div>
+                </div>
+            );
+        }
         return(
-            <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div>
                 {ingredientList}
+                {instructionsList}
             </div>
         );
     }
